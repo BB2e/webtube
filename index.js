96569329e09c8b3fce5cbb0f375d6fe08bff9266
BB2e <79270369+BB2e@users.noreply.github.com>
2022-08-31 02:53:43 +0900

    express app.get route와  req, res

--- index.js
+++ index.js
@@ -3,4 +3,32 @@
 
 import express from "express";
 
-console.log("good")
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+    return res.send('Home!!');
+    // return res.end();
+}
+
+const handleLogin = (req, res) => {
+    return res.send('login page!!');
+}
+
+const handleContact = (req, res) => {
+    return res.send('contact page!!');
+}
+
+const handleAbout = (req, res) => {
+    return res.send('about page!!');
+}
+
+app.get("/", handleHome)
+app.get("/login", handleLogin)
+app.get("/contact", handleContact)
+app.get("/about", handleAbout)
+
+const handleListening = () => console.log(`server listening on port http://localhost:${PORT} 🎉`)
+
+app.listen(PORT, handleListening) // port number, callback
