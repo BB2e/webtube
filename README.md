c5fca3ed178068bd0f38b4c89ef14453a024a9d0
BB2e <seoarum7@daum.net>
2022-09-02 21:23:36 +0900

    router url 별로 분리 정리 전 버전"

---
+++ README.md
@@ -0,0 +1,26 @@
+# Webtube Reloaded
+
+router -> 기능
+
+<!-- # router는 주제 기반 url 그룹화 -->
+
+<!-- global router >> root에 가까운 router 모음 -->
+/ -> Home
+
+/join/ -> Join
+/login/ -> Login
+/search/ -> Search
+
+<!-- user router -->
+/users/edit/ -> Edit user
+/users/delete/ -> Delete user
+
+<!-- video router -->
+/videos/watch/ -> Watch Video
+/videos/edit/ -> Edit video
+/videos/delete/ -> Delete Video
+/videos/comments/ -> comment on a video
+/videos/comments/delete/ -> Delete a comment of a vdieo
+
+
+
--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-// const express = require("express");
-// const app = express();
-
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-    // console.log(`${req.method} ${req.url}`)
-    console.log(`Path: ${req.url}`)
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    // console.log("Access allowed!")
-    next();
-}
-
-const handleHome = (req, res) => {
-    // return res.send('Home!!');
-    return res.end();
-}
-
-const handleProtected = (req, res) => {
-    return res.send('Welcome to private lounge');
-}
-
-// 1. 모든 route에서 쓸수있는 global middleware
+const logger  = morgan("dev");
 app.use(logger)
-app.use(privateMiddleware)
+
+// url 라우터별로 분리해줌
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home")
+globalRouter.get("/", handleHome)
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User")
+userRouter.get("/edit", handleEditUser)
+
+const videoRouter = express.Router();
+const handlewatchVideo = (req, res) => res.send("watch video")
+videoRouter.get("/watch", handlewatchVideo)
+
+
+
+app.use("/", globalRouter)
+app.use("/videos", videoRouter)
+app.use("/users", userRouter)
+
+// const handleHome = (req, res) => {
+//     // return res.send('Home!!');
+//     return res.end();
+// }
+
+// 1. 모든 route에서 쓸수있는 global middleware111
 
 
 // 2. 특정 라우터, middleware, finalware
-// app.get("/", logger, handleHome) 
-app.get("/", handleHome)
-app.get("/protected", handleProtected)
+// app.get("/", handleHome)
 
 const handleListening = () => console.log(`server listening on port http://localhost:${PORT} 🎉`)
 
