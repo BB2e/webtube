263a46bea2e5930f0c31b85b11a14b880ff37fdc
BB2e <79270369+BB2e@users.noreply.github.com>
2022-09-07 23:19:37 +0900

    pug mvp.css

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
-export const trending = (req, res) => res.send("home page videos")
-export const see = (req, res) => {
-    console.log(req.params.id);
-    return res.send("watch")
-}
-export const edit = (req, res) => {
-    console.log(req.params.id);
-    return res.send("edit")
-}
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"})
+// res.render.(템플릿, 보낼변수)
+
+export const see = (req, res) => res.render("watch")
+export const edit = (req, res) => res.render("edit")
 export const search = (req, res) => res.send("search")
 export const upload = (req, res) => res.send("upload")
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -8,6 +8,13 @@ const PORT = 4000;
 
 const app = express();
 const logger  = morgan("dev");
+
+// pug를 사용하는 방법 1. npm i pug 2. app.set 세팅 3.퍼그 파일 만들고 넣기
+app.set("view engine", 'pug') // http://expressjs.com/ko/guide/using-template-engines.html
+app.set("views", process.cwd() + "/src/views") 
+// ㄴ> package.json이 node의 current working directory이기에, 
+// 거기서 views를 찾으면 없어서
+// 현재 pug가 있는 views 폴더를 알려준다
 app.use(logger)
 
 app.use("/", globalRouter)
